191c97827b59e1cfdcf9e44ce18d239a9cce80ae
Matthews Britto <luismatthews29@gmail.com>
2023-11-10 16:55:56 -0300

    'fix:PortfolioRequest'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import About from '@/components/About'
 import Home from '@/components/Home'
 import Skills from '@/components/Skills'
-import Works from '@/components/Works'
+import { Works } from '@/components/Works'
 
 export default function page() {
   return <Works />
